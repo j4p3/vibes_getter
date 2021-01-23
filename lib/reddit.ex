5a559660fc502744a0973a7dd3132659ea3f6316
JP <jp.bonner@bonner.jp>
2021-01-23 15:49:49 -0700

    handle media sources

--- lib/reddit.ex
+++ lib/reddit.ex
@@ -3,6 +3,9 @@ defmodule Reddit do
   Handler for Reddit vibes
   """
 
+  IMGUR_POST_URI_PATTERN = ~r/[a-z]\/(.*)$/
+  GYFCAT_POST_URI_PATTERN = ~r/[a-z]\/(.*)\-size_restricted.gif$/
+
   # @spec vibe_from_post(Map) :: VibesGetter.vibe_resonator
   def vibe_from_post(post) do
     post_data = post["data"]
@@ -17,25 +20,28 @@ defmodule Reddit do
 
   @spec media_url_for_post(Map) :: charlist
   defp media_url_for_post(post_data) do
-
-  end
-
-  @spec post_source(Map) :: atom
-  defp post_source(post_data) do
     case post_data["domain"] do
       "imgur.com" ->
-        # @todo regex out uri + append extensions until something works
         Map.get(post_data, "url_overridden_by_dest")
+        |> extract_substring(IMGUR_POST_URI_PATTERN)
+        |> &("https://i.imgur.com/#{&1}.jpg")
+        # @todo: check response headers for filetype
       "i.imgur.com" ->
         Map.get(post_data, "url_overridden_by_dest")
       "i.redd.it" ->
         Map.get(post_data, "url_overridden_by_dest")
       "gfycat.com" ->
-        # @todo regex out uri frmo thumnail_url
-        "https://giant.gyfcat.com/#{}.mp4"
+        get_in(post_data, "secure_media", "oembed", "thumbnail_url")
+        |> extract_substring(GYFCAT_POST_URI_PATTERN)
+        |> &("https://giant.gyfcat.com/#{&1}.mp4")
       "v.redd.it" ->
         get_in(post_data, "secure_media", "reddit_video", "fallback_url")
     end
+  end
+
+  defp extract_substring(target_string, regex_pattern) do
+    Regex.run(regex_pattern, target_string, [capture: :all_but_first])
+    |> List.first()
   end
 
   @spec type_for_post(Map) :: atom
@@ -45,8 +51,8 @@ defmodule Reddit do
         :video
       post_data["is_meta"] ->
         :text
-      post_data["is_media"] ->
-        nil
+      post_data["is_self"] ->
+        :text
     end
   end
 end
